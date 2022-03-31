2cb43c2e63bb333973912e8a969f07cf6195f49f
Leonardo Lima <leonardo.lima@intelbras.com.br>
2022-03-30 23:07:50 -0300

    Commit in branch1

--- Excel.py
+++ Excel.py
@@ -15,13 +15,14 @@ for colaborador in df_escala.index:
     status = df_escala.loc[colaborador][date_today]
     dict_schedule[colaborador] = status
 
+print(dict_schedule)
 
 
 def send_email(dict_escala, date_to_insert):
     username = "le052437@intelbras.com.br"
     password = "Asadew12w1"
     mail_from = "le052437@intelbras.com.br"
-    mail_to = "leonardo.lima@intelbras.com.br"
+    mail_to = "," .join(["leonardo.lima@intelbras.com.br", "limarodriguesleonardo@gmail.com"])
     mail_subject = "Controle de Escala"
 
     mimemsg = MIMEMultipart()
@@ -37,8 +38,69 @@ def send_email(dict_escala, date_to_insert):
     <body>
     <p>Bom dia,</p>
     <p>Segue a seguir a escala programada da equipe para hoje {date}:</p>
-    <h2>Controle de Escala</h2>
-    <p>{escala}</p>
+    <h2>Controle de Escala</h2>    
+    <table border="1">
+    <tr>
+        <td> Nome </td>
+        <td> Escala </td>        
+    </tr>
+    <tr>
+        <td> {colaborador1} </td>
+        <td> {status_colaborador1} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador2} </td>
+        <td> {status_colaborador2} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador3} </td>
+        <td> {status_colaborador3} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador4} </td>
+        <td> {status_colaborador4} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador5} </td>
+        <td> {status_colaborador5} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador6} </td>
+        <td> {status_colaborador6} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador7} </td>
+        <td> {status_colaborador7} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador8} </td>
+        <td> {status_colaborador8} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador9} </td>
+        <td> {status_colaborador9} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador10} </td>
+        <td> {status_colaborador10} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador11} </td>
+        <td> {status_colaborador11} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador12} </td>
+        <td> {status_colaborador12} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador13} </td>
+        <td> {status_colaborador13} </td>        
+    </tr>
+    <tr>
+        <td> {colaborador14} </td>
+        <td> {status_colaborador14} </td>        
+    </tr>  
+    </table>    
     <p></p>
     <p> Segue o link para acesso a planilha: https://onedrive.live.com/edit.aspx?cid=24ecdaf014356d2c&page=view&resid=24ECDAF014356D2C!58898&parId=24ECDAF014356D2C!101&app=Excel </p>
     <p></p>
@@ -47,7 +109,35 @@ def send_email(dict_escala, date_to_insert):
     </body>
     </html>
         """.format(date=date_to_insert,
-                   escala=dict_escala
+                   escala=dict_escala,
+                   colaborador1="Stefany",
+                   status_colaborador1=dict_schedule["Stefany"],
+                   colaborador2="Marcos",
+                   status_colaborador2=dict_schedule["Marcos"],
+                   colaborador3="Ana",
+                   status_colaborador3=dict_schedule["Ana"],
+                   colaborador4="Mendonça",
+                   status_colaborador4=dict_schedule["Mendonça"],
+                   colaborador5="Ruth",
+                   status_colaborador5=dict_schedule["Ruth"],
+                   colaborador6="Gabriel",
+                   status_colaborador6=dict_schedule["Gabriel"],
+                   colaborador7="Gustavo",
+                   status_colaborador7=dict_schedule["Gustavo"],
+                   colaborador8="Maykon",
+                   status_colaborador8=dict_schedule["Maykon"],
+                   colaborador9="Maitê",
+                   status_colaborador9=dict_schedule["Maitê"],
+                   colaborador10="Guilherme",
+                   status_colaborador10=dict_schedule["Guilherme"],
+                   colaborador11="Elionai",
+                   status_colaborador11=dict_schedule["Elionai"],
+                   colaborador12="Priscila",
+                   status_colaborador12=dict_schedule["Priscila"],
+                   colaborador13="Leo",
+                   status_colaborador13=dict_schedule["Leo"],
+                   colaborador14="Lucas Ribas",
+                   status_colaborador14=dict_schedule["Lucas Ribas"],
                    )
 
     # Turn these into plain/html MIMEText objects
